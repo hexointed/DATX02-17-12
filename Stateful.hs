a0735923f9c50027e367fb4c7cb332dc18e141ee
Elias Forsberg <forsberg.elias@gmail.com>
2017-03-28 16:30:52 +0200

    Add DFU testing

--- Stateful.hs
+++ Stateful.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
+
 module Stateful where
 
 import CLaSH.Prelude
+import Base
 
 data State a
 	= Result a -- A result is available
 	| Ready    -- Ready to start a new batch
 	| WaitI    -- Ready for next instruction
+	deriving (Eq, Show, Generic, NFData)
 
 class Stateful a where
 	type Out a :: *
