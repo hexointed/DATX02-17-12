69cfbca1258c5fe50d3a948bbe70c66e794dd792
Elias Forsberg <forsberg.elias@gmail.com>
2017-03-29 11:58:03 +0200

    Fix imports

--- Memory.hs
+++ Memory.hs
@@ -1,6 +1,13 @@
 module Memory where
 
 import Core
+import Base
+import Indexed
+import Pack
+import DFU
+import CFU
+import DistFunc
+import Float
 
 dfuIMemory :: Ptr DIMem -> Maybe Reset
 dfuIMemory = encodeDfuI asyncRomFile d512 "dfuIMemory.bin"
@@ -10,18 +17,12 @@ cfuIMemory :: Ptr CIMem -> Maybe Instr
 cfuIMemory = encodeCfuI asyncRomFile d512 "cfuIMemory.bin"
 
 fetch :: CoreOut -> CoreIn 
-fetch CO = CoreIn { nextPack=Nothing
-           , dfuInstr=dfuIMemory (dfuIPtr CO)
-           , dfuData=dfuDMemory (dfuDPtr CO)
-           , cfuInstr=cfuIMemory (cfuIPtr CO)
+fetch co = CoreIn { nextPack=Nothing
+           , dfuInstr=dfuIMemory (dfuIPtr co)
+           , dfuData=dfuDMemory (dfuDPtr co)
+           , cfuInstr=cfuIMemory (cfuIPtr co)
            }
 
 encodeDfuI :: BitVector 68 -> Maybe Reset
 encodeDfuD :: BitVector 64 -> Maybe Float
-encodeDfuI :: BitVector 68 -> Maybe Instr
-
-
-
-
-
-
+encodeCfuI :: BitVector 68 -> Maybe Instr
