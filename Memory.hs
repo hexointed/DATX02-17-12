2225685a38ce9e89e8a95f336933cac132a14793
Björn Strömberg <bjeeeern@gmail.com>
2017-03-29 11:54:26 +0200

    finishes of topqueue par FrameBuffer

--- Memory.hs
+++ Memory.hs
@@ -2,13 +2,26 @@ module Memory where
 
 import Core
 
-dfuIMemory = asyncRomFile d512 "dfuIMemory.bin"
-dfuDMemory = asyncRomFile d512 "dfuDMemory.bin"
-cfuIMemory = asyncRomFile d512 "cfuIMemory.bin"
+dfuIMemory :: Ptr DIMem -> Maybe Reset
+dfuIMemory = encodeDfuI asyncRomFile d512 "dfuIMemory.bin"
+dfuDMemory :: Ptr DDMem -> Maybe Float
+dfuDMemory = encodeDfuD asyncRomFile d512 "dfuDMemory.bin"
+cfuIMemory :: Ptr CIMem -> Maybe Instr
+cfuIMemory = encodeCfuI asyncRomFile d512 "cfuIMemory.bin"
 
 fetch :: CoreOut -> CoreIn 
-fetch CO = { nextPack=Nothing
+fetch CO = CoreIn { nextPack=Nothing
            , dfuInstr=dfuIMemory (dfuIPtr CO)
            , dfuData=dfuDMemory (dfuDPtr CO)
            , cfuInstr=cfuIMemory (cfuIPtr CO)
            }
+
+encodeDfuI :: BitVector 68 -> Maybe Reset
+encodeDfuD :: BitVector 64 -> Maybe Float
+encodeDfuI :: BitVector 68 -> Maybe Instr
+
+
+
+
+
+
--- TopQueue.hs
+++ TopQueue.hs
@@ -1,14 +1,21 @@
 module TopQueue where
 
+import Base
 import Core
+import Pack
 import Queue
 
-initTopQueue = undefined
-
-serve :: CoreOut -> Queue -> (CoreIn, Queue)
-serve CO Q = ({ nextPack=np' ()()()
-              , dfuInstr=0
-              , dfuData=0
-              , cfuInstr=0
-              }, Q')
+serve :: CoreOut -> Queue 64 Pack -> (CoreIn, Queue 64 Pack)
+serve co q = (CoreIn { nextPack=np' (ready co)
+              , dfuInstr=Nothing
+              , dfuData=Nothing
+              , cfuInstr=Nothing
+              }, q' (packOut co) (packType co) (ready co))
     where
+    np' True = Just (top q)
+    np' _  = Nothing
+    q' _ _ True = pop q
+    q' p None _ = q
+    q' p Core.Queue _ = push p q
+    --TODO(bjorn): Add memory to write frame to.
+    q' p Frame _ = undefined 
