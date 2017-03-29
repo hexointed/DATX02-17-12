ae20b5a58b154629aa4203eac3f7359202f2dbef
Björn Strömberg <bjeeeern@gmail.com>
2017-03-29 18:38:03 +0200

    fucks up queue.hs

--- TopQueue.hs
+++ TopQueue.hs
@@ -5,17 +5,37 @@ import Core
 import Pack
 import Queue
 
-serve :: CoreOut -> Queue 64 Pack -> (CoreIn, Queue 64 Pack)
-serve co q = (CoreIn { nextPack=np' (ready co)
-              , dfuInstr=Nothing
-              , dfuData=Nothing
-              , cfuInstr=Nothing
-              }, q' (packOut co) (packType co) (ready co))
+width = d16
+height = d16 
+frameBuffer = asyncRam (width `mulSNat` height)
+
+type QueueState = (Queue 64 Pack, Signal Value)
+
+serve :: CoreOut -> QueueState -> (CoreIn, QueueState)
+serve co fb (q, s) = ( CoreIn { nextPack=np' (ready co)
+                           , dfuInstr=Nothing
+                           , dfuData=Nothing
+                           , cfuInstr=Nothing
+                           }
+                   , ( q' (packOut co) (packType co) (ready co)
+                     , s' (packOut co) (packType co) 
+                     )
+                   )
     where
     np' True = Just (top q)
     np' _  = Nothing
     q' _ _ True = pop q
-    q' p None _ = q
     q' p Core.Queue _ = push p q
-    --TODO(bjorn): Add memory to write frame to.
-    q' p Frame _ = undefined 
+    q' _ _ _ = q
+    s' (col, pos) Frame = frameBuffer (Signal addr) (Signal (Just (addr, color))) 
+    s' (col, pos) _ = frameBuffer (Signal addr) (Signal Nothing) 
+        where
+        x = (pos !! 0)
+        y = (pos !! 1)
+        addr = ((natVal width) * y + x)
+        color = 
+
+
+
+
+
