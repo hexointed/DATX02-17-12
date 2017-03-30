42b8a5ec6c25b4a9ef0a05b2219a3eab2f02fdf4
Björn Strömberg <bjeeeern@gmail.com>
2017-03-30 14:49:05 +0200

    working on the queue

--- TopQueue.hs
+++ TopQueue.hs
@@ -1,41 +1,45 @@
+{-# LANGUAGE MagicHash #-}
+
 module TopQueue where
 
 import Base
 import Core
 import Pack
 import Queue
+import CLaSH.Sized.Fixed
+import CLaSH.Sized.Internal.Signed
+import CLaSH.Sized.BitVector
 
 width = d16
-height = d16 
+height = d16
+
 frameBuffer = asyncRam (width `mulSNat` height)
 
-type QueueState = (Queue 64 Pack, Signal Value)
-
-serve :: CoreOut -> QueueState -> (CoreIn, QueueState)
-serve co fb (q, s) = ( CoreIn { nextPack=np' (ready co)
-                           , dfuInstr=Nothing
-                           , dfuData=Nothing
-                           , cfuInstr=Nothing
-                           }
-                   , ( q' (packOut co) (packType co) (ready co)
-                     , s' (packOut co) (packType co) 
-                     )
-                   )
-    where
-    np' True = Just (top q)
-    np' _  = Nothing
-    q' _ _ True = pop q
-    q' p Core.Queue _ = push p q
-    q' _ _ _ = q
-    s' (col, pos) Frame = frameBuffer (Signal addr) (Signal (Just (addr, color))) 
-    s' (col, pos) _ = frameBuffer (Signal addr) (Signal Nothing) 
-        where
-        x = (pos !! 0)
-        y = (pos !! 1)
-        addr = ((natVal width) * y + x)
-        color = 
-
-
-
-
-
+serve :: CoreOut -> Queue 64 Pack -> (CoreIn, Queue 64 Pack, Signal (BitVector d24))
+serve ( CoreOut { packOut  = p
+                , packType = pt
+                , ready    = rdy
+                } 
+      ) q = ( CoreIn { nextPack  = np' rdy
+                      , dfuInstr = Nothing
+                      , dfuData  = Nothing
+                      , cfuInstr = Nothing
+                      }
+            , q' pt rdy
+            , s' pt
+            )
+  where
+    np' True        = Just (top q)
+    np' _           = Nothing
+    q' _ True       = pop q
+    q' Core.Queue _ = push p q
+    q' _ _          = q
+    s' Frame        = frameBuffer (signal addr) (signal (Just (addr, color))) 
+    s' _            = frameBuffer (signal addr) (signal Nothing) 
+    color           = r ++# g ++# b
+    r               = bSlice d32 d40 $ pack# $ unSF (p !! 2)
+    g               = bSlice d32 d40 $ pack# $ unSF (p !! 3)
+    b               = bSlice d32 d40 $ pack# $ unSF (p !! 4)
+    addr            = (natVal width) * y + x
+    x               = toInteger# $ unpack# $ bSlice d32 d64 $ pack# $ unSF (p !! 0)
+    y               = toInteger# $ unpack# $ bSlice d32 d64 $ pack# $ unSF (p !! 1)
