97a25bc6dccb2b0202b9e31c4783f61611e8c6f5
Elias Forsberg <forsberg.elias@gmail.com>
2017-04-04 13:16:21 +0200

    Rewrite TopQueue

--- TopQueue.hs
+++ TopQueue.hs
@@ -12,29 +12,12 @@ type TopQueue = Queue 64 Pack
 type Address = Signed 32
 type RGBValue = BitVector 24
 
-serve :: CoreOut -> TopQueue -> (CoreIn, TopQueue, Address, Maybe (Address, RGBValue))
-serve (CoreOut _ _ _ p pt rdy) q = 
-			( CoreIn { nextPack  = np' rdy
-                      , dfuInstr = Nothing
-                      , dfuData  = Nothing
-                      , cfuInstr = Nothing
-                      }
-            , q' pt rdy
-            , addr
-            , px pt
-            )
-  where
-    np' True        = Just (top q)
-    np' _           = Nothing
-    q' _ True       = pop q
-    q' Core.Queue _ = push p q
-    q' _ _          = q
-    px Frame        = Just (addr, color) 
-    px _            = Nothing 
-    addr            = (16::Signed 32) * y + x
-    x               = unpack $ bSlice d0 d32 $ pack $ unSF $ (p !! 0)
-    y               = unpack $ bSlice d0 d32 $ pack $ unSF $ (p !! 1) :: Signed 32
-    color           = bSlice d8 d24 (r+g+b)
-    r               = bSlice d0 d32 $ pack $ unSF $ (p !! 2)
-    g               = flip shiftL 8 $ bSlice d0 d32 $ pack $ unSF $ (p !! 3)
-    b               = flip shiftL 16 $ bSlice d0 d32 $ pack $ unSF $ (p !! 4)
+serve :: TopQueue -> (Bool, Maybe Pack) -> (TopQueue, Maybe Pack)
+serve q (rdy, pack) = mpop $ psh q
+	where
+		mpop = case rdy of
+			True  -> (\q -> (pop q, Just $ top q))
+			False -> (\q -> (q, Nothing))
+		psh = case pack of
+			Just p  -> push p
+			Nothing -> id
