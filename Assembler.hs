0ecc2a30056cc22455ac9046b7fbba1638d6bee7
Elias Forsberg <forsberg.elias@gmail.com>
2017-04-07 12:51:56 +0200

    Update Assembler to read entire files

--- Assembler.hs
+++ Assembler.hs
@@ -13,28 +13,35 @@ zeroes n = take n $ repeat '0'
 
 assemble f = concat . fmap ((++"\n") . (concat . fmap f . words)) . lines
 
+asm f = case dropWhile (/='.') f of
+	".dasm" -> asm' f assembleDI
+	".casm" -> asm' f assembleCI
+	_       -> error "Unrecognized file extension"
+
+asm' :: String -> (String -> String) -> IO ()
+asm' f ass = do
+	f' <- readFile f
+	writeFile (takeWhile (/='.') f ++ ".bin") (ass f')
+
 assembleDI :: String -> String
 assembleDI = assemble wtb
 	where
 		wtb w 
-			| w == "Continue" = "00"
-			| w == "Next" = "01"
-			| w == "Compute" = "10" ++ zeroes 34
-			| w == "Done" = "11" ++ zeroes 34
-			| w == "Left" = "0"
-			| w == "Right" = "1"
-			| w == "Max" = "000" ++ zeroes 30
-			| w == "Min" = "001" ++ zeroes 30
-			| w == "Add" = "010" ++ zeroes 30
-			| w == "Sub" = "011" ++ zeroes 30
-			| w == "Mul" = "100" ++ zeroes 30
-			| w == "Div" = "101" ++ zeroes 30
-			| w == "Sqrt" = "110" ++ zeroes 30
-			| w == "Abs" = "111" ++ zeroes 30
-			| w == "Val" = "0"
-			| w == "Arg" = "1"
+			| w == "Next"     = "01"
+			| w == "Compute"  = "10" ++ zeroes 34
+			| w == "Done"     = "11" ++ zeroes 34
+			| w == "Max"  = "000000" ++ zeroes 30
+			| w == "Min"  = "000001" ++ zeroes 30
+			| w == "Add"  = "000010" ++ zeroes 30
+			| w == "Sub"  = "000011" ++ zeroes 30
+			| w == "Mul"  = "000100" ++ zeroes 30
+			| w == "Div"  = "000101" ++ zeroes 30
+			| w == "Sqrt" = "000110" ++ zeroes 30
+			| w == "Abs"  = "000111" ++ zeroes 30
+			| w == "Val"  = "0010"
+			| w == "Arg"  = "0011"
 			| "pp" `isInfixOf` w = (u3ToBinary $ drop 2 w) ++ zeroes 29
-			| "id" `isInfixOf` w = zeroes 18 ++ (u16ToBinary $ drop 2 w)
+			| "id" `isInfixOf` w = (u16ToBinary $ drop 2 w) ++ zeroes 18
 			| otherwise = rationalToBinary w
 
 assembleDD :: String -> String
@@ -46,19 +53,16 @@ assembleCI :: String -> String
 assembleCI = assemble wtb
 	where
 		wtb w 
-			| w == "Instr" = ""
-			| w == "Cond" = ""
 			| w == "NZ" = "00"
-			| w == "Z" = "01"
-			| w == "A" = "10"
-			| w == "PushF" = "00"
-			| w == "PushQ" = "01"
-			| w == "Drop" = "10"
+			| w == "Z"  = "01"
+			| w == "A"  = "100000"
+			| w == "PushF" = "00" ++ zeroes 8
+			| w == "PushQ" = "01" ++ zeroes 8
+			| w == "Drop"  = "10" ++ zeroes 8
 			| w == "SetVal" = "11"
 			| "cp" `isInfixOf` w = (u4ToBinary $ drop 2 w)
 			| "pp" `isInfixOf` w = (u3ToBinary $ drop 2 w) ++ zeroes 1
 			| "sp" `isInfixOf` w = (u4ToBinary $ drop 2 w)
-			| "_" `isInfixOf` w = zeroes 10
 
 clashToBin = filter (/= '_') . show . pack
 
