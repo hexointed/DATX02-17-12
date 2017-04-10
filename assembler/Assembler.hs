6336a2dc61497441c8bbad61895b1a2e4504c00e
Elias Forsberg <forsberg.elias@gmail.com>
2017-04-10 15:04:00 +0200

    Split assembler files

---
+++ assembler/Assembler.hs
@@ -0,0 +1,72 @@
+module Assembler where
+
+import CLaSH.Prelude hiding ((++), last, tail, Word, lines, zip, map, Float)
+import Data.List
+import Data.Char
+import Prelude hiding (Word, Float)
+
+import Inst
+
+data TextType = Data [[String]] | Code [[String]]
+type Float = Fixed Signed 16 16
+
+break = fmap words . lines . map toLower
+glue = unlines . fmap unwords
+
+pieceUp :: [[String]] -> [TextType]
+pieceUp ((".data":ws):ls) = (Data (findThisPiece ls)):(pieceUp ls)
+pieceUp ((".text":ws):ls) = (Code (findThisPiece ls)):(pieceUp ls)
+pieceUp (l:ls) = pieceUp ls
+pieceUp [] = []
+
+findThisPiece :: [[String]] -> [[String]]
+findThisPiece ((".data":ws):ls) = []
+findThisPiece ((".text":ws):ls) = []
+findThisPiece (([]):ls) = findThisPiece ls
+findThisPiece (l:ls) = l:(findThisPiece ls)
+findThisPiece [] = []
+
+getGotos :: Int -> Int -> [TextType] -> [(String,Int)]
+getGotos dln cln ((Data ls):ts) = gt ++ (getGotos nln cln ts)
+	where (nln, gt) = makeGotos dln ls
+getGotos dln cln ((Code ls):ts) = gt ++ (getGotos dln nln ts)
+	where (nln, gt) = makeGotos cln ls
+getGotos _ _ [] = []
+
+makeGotos ln ls = (nln, gt)
+	where 
+		(_,nln) = last gt
+		gt = makeGotos' ln ls
+makeGotos' :: Int -> [[String]] -> [(String,Int)]
+makeGotos' ln ((w:[]):l:ls)| isGoto w = (w \\ ":", ln):(makeGotos' (ln+1) ls)
+makeGotos' ln (l:ls) = (makeGotos' (ln+1) ls)
+makeGotos' _ [] = []
+isGoto w = ":" `isInfixOf` w
+
+onlyData :: [TextType] -> [[String]]
+onlyData ((Data ls):ts) = ls ++ (onlyData ts)
+onlyData (_:ts) = onlyData ts
+onlyData [] = []
+onlyCode :: [TextType] -> [[String]]
+onlyCode ((Code ls):ts) = ls ++ (onlyCode ts)
+onlyCode (_:ts) = onlyCode ts
+onlyCode [] = []
+
+replaceGotos :: [(String,Int)] -> [[String]] -> [[String]]
+replaceGotos gt ((ws):ls) = (fmap (\w -> if isInfixOf "&" w then show $ lookup (w \\ "&") gt else w) ws):(replaceGotos gt ls)
+
+makeData :: [[String]] -> String
+makeData = glue . fmap (fmap (\w -> if not $ isGoto w then datToBin w else w))
+
+
+datToBin :: String -> String
+datToBin w 
+	| ("." `isInfixOf` w || "e" `isInfixOf` w) = rationalToBinary w 
+	| "-" `isInfixOf` w = s32ToBinary w
+        | otherwise = u32ToBinary w
+
+s32ToBinary = clashToBin . (read :: String -> Signed 32) 
+u32ToBinary = clashToBin . (read :: String -> Unsigned 32) 
+rationalToBinary = clashToBin . (fLitR :: Double -> Float) . read
+
+clashToBin = filter (/= '_') . show . pack
--- assembler/Inst.hs
+++ assembler/Inst.hs
@@ -1,76 +1,12 @@
 {-# LANGUAGE MultiWayIf #-}
 
-module Assembler where
+module Inst 
+		(Bits', Line, Word, cleanupCode, assembleInst) 
+	where
 
-import CLaSH.Prelude hiding ((++), last, tail, Word, lines, zip, map)
+import CLaSH.Prelude hiding (tail, Word, lines, zip, map)
 import Data.List
-import Data.Char
-import qualified Float
 import Prelude hiding (Word)
-
-data TextType = Data [[String]] | Code [[String]]
-
-break = fmap words . lines . map toLower
-glue = unlines . fmap unwords
-
-pieceUp :: [[String]] -> [TextType]
-pieceUp ((".data":ws):ls) = (Data (findThisPiece ls)):(pieceUp ls)
-pieceUp ((".text":ws):ls) = (Code (findThisPiece ls)):(pieceUp ls)
-pieceUp (l:ls) = pieceUp ls
-pieceUp [] = []
-
-findThisPiece :: [[String]] -> [[String]]
-findThisPiece ((".data":ws):ls) = []
-findThisPiece ((".text":ws):ls) = []
-findThisPiece (([]):ls) = findThisPiece ls
-findThisPiece (l:ls) = l:(findThisPiece ls)
-findThisPiece [] = []
-
-getGotos :: Int -> Int -> [TextType] -> [(String,Int)]
-getGotos dln cln ((Data ls):ts) = gt ++ (getGotos nln cln ts)
-	where (nln, gt) = makeGotos dln ls
-getGotos dln cln ((Code ls):ts) = gt ++ (getGotos dln nln ts)
-	where (nln, gt) = makeGotos cln ls
-getGotos _ _ [] = []
-
-makeGotos ln ls = (nln, gt)
-	where 
-		(_,nln) = last gt
-		gt = makeGotos' ln ls
-makeGotos' :: Int -> [[String]] -> [(String,Int)]
-makeGotos' ln ((w:[]):l:ls)| isGoto w = (w \\ ":", ln):(makeGotos' (ln+1) ls)
-makeGotos' ln (l:ls) = (makeGotos' (ln+1) ls)
-makeGotos' _ [] = []
-isGoto w = ":" `isInfixOf` w
-
-onlyData :: [TextType] -> [[String]]
-onlyData ((Data ls):ts) = ls ++ (onlyData ts)
-onlyData (_:ts) = onlyData ts
-onlyData [] = []
-onlyCode :: [TextType] -> [[String]]
-onlyCode ((Code ls):ts) = ls ++ (onlyCode ts)
-onlyCode (_:ts) = onlyCode ts
-onlyCode [] = []
-
-replaceGotos :: [(String,Int)] -> [[String]] -> [[String]]
-replaceGotos gt ((ws):ls) = (fmap (\w -> if isInfixOf "&" w then show $ lookup (w \\ "&") gt else w) ws):(replaceGotos gt ls)
-
-makeData :: [[String]] -> String
-makeData = glue . fmap (fmap (\w -> if not $ isGoto w then datToBin w else w))
-
-
-datToBin :: String -> String
-datToBin w 
-	| ("." `isInfixOf` w || "e" `isInfixOf` w) = rationalToBinary w 
-	| "-" `isInfixOf` w = s32ToBinary w
-        | otherwise = u32ToBinary w
-
-s32ToBinary = clashToBin . (read :: String -> Signed 32) 
-u32ToBinary = clashToBin . (read :: String -> Unsigned 32) 
-rationalToBinary = clashToBin . (fLitR :: Double -> Float.Float) . read
-
-clashToBin = filter (/= '_') . show . pack
--- =======
 
 type Bits' n = Either String (BitVector n)
 type Line = String
