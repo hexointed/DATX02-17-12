5dbd1b108892b9a89d8abcc5261d9236ab7131a1
Elias Forsberg <forsberg.elias@gmail.com>
2017-04-10 13:42:47 +0200

    Write new Assembler

--- Assembler.hs
+++ Assembler.hs
@@ -1,88 +1,99 @@
+{-# LANGUAGE MultiWayIf #-}
 
+module Assembler where
 
-module Assembler (asm) where
+import CLaSH.Prelude hiding (tail)
+import Prelude (tail)
 
-import qualified Float
-import CLaSH.Sized.Unsigned
-import CLaSH.Class.BitPack
-import CLaSH.Sized.Fixed
-import CLaSH.Sized.Internal.Signed
-import Data.String
-import Data.List
-import qualified Prelude (Float)
-import Prelude hiding (Float) 
+type Bits' n = Either String (BitVector n)
 
-zeroes n = take n $ repeat '0'
+assembleInst :: String -> Bits' 16
+assembleInst str = case words str of
+	[]   -> Left "Expected instruction, found nothing"
+	w:ws -> if
+		| isCond w    -> assembleCFU (w:ws)
+		| w == "next" -> assembleNext ws
+		| w == "val"  -> assembleVal ws
+		| w == "pack" -> assemblePack ws
+		| otherwise   -> assembleDFU (w:ws)
 
-asm f = case dropWhile (/='.') f of
-	".dasm" -> asm' f assembleDI
-	".masm" -> asm' f assembleDD
-	_       -> error "Unrecognized file extension"
-
-asm' :: String -> (String -> String) -> IO ()
-asm' f ass = do
-	f' <- readFile f
-	writeFile (takeWhile (/='.') f ++ ".bin") (ass f')
-
-assemble f s = fWordsInSrc (f gotos) s'
+assembleCFU :: [String] -> Bits' 16
+assembleCFU ws = case ws of
+	"a"   :inst -> ass "a" "0"  inst
+	c:cptr:inst -> ass c   cptr inst
 	where
-		gotos = fmap (\(ln,l) -> (ln, drop 1 . concat . take 1 . takeWhile (isInfixOf "#") . words $ l)) (zipWith [0..] $ lines s)
-		s' = fWordsInSrc (rmCmm . takeWhile (not . isInfixOf "#")) s
-		rmCmm w:sw = if not $ isInfixOf ";" w then w:(rmCmm sw) else ""
+		ass a b c = do
+			con <- cond a
+			ptr <- rptr b
+			tra <- tran c
+			return $ con ++# ptr ++# tra
 
-assembleDI :: String -> String
-assembleDI = assemble ltb
+		tran :: [String] -> Bits' 10
+		tran ws = do
+			name <- head' ws
+			case name of
+				"pushf"  -> return 0
+				"pushq"  -> return 1
+				"setval" -> do
+					a1 <- head' (tail ws)
+					a2 <- head' (tail $ tail ws)
+					n1 <- readUnsigned d4 a1
+					n2 <- readUnsigned d4 a2
+					return $ 2 ++# n1 ++# n2
+				"drop"   -> return 3
+				_        -> Left "Unrecognized instruction"
+		
+		rptr :: String -> Bits' 4
+		rptr = readUnsigned d4
+		
+		cond :: String -> Bits' 2
+		cond "a"  = Right 0
+		cond "z"  = Right 1
+		cond "nz" = Right 2
+
+assembleVal ws = do
+	n <- single ws
+	n <- readUnsigned d12 n
+	return $ 0xD ++# n 
+
+assemblePack ws = do
+	n <- single ws
+	n <- readUnsigned d12 n
+	return $ 0xC ++# n
+
+assembleNext ws = do
+	n <- single ws
+	n <- readUnsigned d12 n
+	return $ 0xE ++# n
+
+assembleDFU ws = do
+	n <- single ws
+	i <- case n of
+		"max"   -> Right 0
+		"min"   -> Right 1
+		"add"   -> Right 2
+		"sub"   -> Right 3
+		"mul"   -> Right 4
+		"div"   -> Right 5
+		"sqrt"  -> Right 6
+		"abs"   -> Right 7
+		"floor" -> Right 8
+		_       -> Left "Unrecognized instruction"
+	return $ (2 :: BitVector 2) ++# (i :: BitVector 6) ++# 0
+
+readUnsigned s n = fmap pack $ read' s n 
 	where
-		ltb "nz":w2:"SetVal":w4:w5:[] gt = "0000" ++ cp ++ "11" ++ rp ++ sp
-		ltb "z" :w2:"SetVal":w4:w5:[] gt = "0001" ++ cp ++ "11" ++ rp ++ sp
-			rp = (numToBin w3 :: KnownNat 3) ++ "0"
-			sp = (numToBin w4 :: KnownNat 4)
-			cp = (numToBin w4 :: KnownNat 4)
-		ltb "a":"SetVal":w3:w4:[] gt = "0010000011" ++ rp ++ sp 
-			rp = (numToBin w3 :: KnownNat 3) ++ "0"
-			sp = (numToBin w4 :: KnownNat 4)
-		ltb "nz":w2:w3:[] gt = "0000" ++ cp ++ act w3
-		ltb "z" :w2:w3:[] gt = "0001" ++ cp ++ act w3
-			cp = (numToBin w2 :: KnownNat 4)
-			act "PushQ" = "1010101"
-			act "PushF" = "1010101"
-			act "Drop" = "1010101"
-		ltb "next":w2:[] gt = "01" ++ idp
-		ltb "val" :w2:[] gt = "0010" ++ val
-		ltb "arg" :w2:[] gt = "0011" ++ pp
-		ltb "a"   :w2:[] gt = "00100000" ++ act
-			idp = (numToBin w2 :: KnownNat 3) ++ zeroes 11
-			val = (numToBin w2 :: KnownNat 32)
-			pp = (numToBin w2 :: KnownNat 16)
-			act "PushQ" = "1010101"
-			act "PushF" = "1010101"
-			act "Drop" = "1010101"
-		ltb "max"  :[] _ = "0000000" ++ zeroes 29
-		ltb "min"  :[] _ = "0000001" ++ zeroes 29
-		ltb "add"  :[] _ = "0000010" ++ zeroes 29
-		ltb "sub"  :[] _ = "0000011" ++ zeroes 29
-		ltb "mul"  :[] _ = "0000100" ++ zeroes 29
-		ltb "div"  :[] _ = "0000101" ++ zeroes 29
-		ltb "sqrt" :[] _ = "0000110" ++ zeroes 29
-		ltb "abs"  :[] _ = "0000111" ++ zeroes 29
-		ltb "floor":[] _ = "0001000" ++ zeroes 29
+		read' :: KnownNat n => SNat n -> String -> Either String (Unsigned n)
+		read' _ n = case reads n of
+			[(n', "")] -> Right n'
+			[]         -> Left "Expected number"
 
-numToBin :: KnownNat a => String -> String
-numToBin w 
-	| ("." `isInfixOf` w || "e" `isInfixOf` w) = rationalToBinary w 
-	| "-" `isInfixOf` w = sNToBinary w :: Signed a
-        | otherwise = uNToBinary w :: Unsigned a
+isCond w = case filter (==w) ["a", "nz", "z"] of
+	[] -> False
+	_  -> True
 
-sNToBinary = clashToBin . (read :: String -> Signed a) 
-uNToBinary = clashToBin . (read :: String -> Unsigned a) 
-rationalToBinary = clashToBin . (fLitR :: Double -> Float.Float) . read
+single (l:[]) = Right l
+single _      = Left "Wrong number of arguments"
 
-clashToBin = filter (/= '_') . show . pack
-
-assembleDD :: String -> String
-assembleDD = assemble wtb
-	where
-		wtb w
-			| ("." `isInfixOf` w || "e" `isInfixOf` w) = rationalToBinary w 
-			| "-" `isInfixOf` w = s32ToBinary w 
-			| otherwise = u32ToBinary w 
+head' (l:ls) = Right l
+head' _      = Left "Not enough arguments"
