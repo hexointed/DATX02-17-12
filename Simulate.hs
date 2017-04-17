4bd530071d7b150af4f1e85a2857e859c7d3cb5b
Jon Johnsson <me@jonjohnsson.com>
2017-04-17 13:48:54 +0200

    added a clearscreen-render variant of  simGPU

--- Simulate.hs
+++ Simulate.hs
@@ -4,6 +4,7 @@ import Base
 import Framebuffer
 import GPU
 import qualified Prelude as P
+import System.Console.ANSI
 
 showFrame = 
 	fmap show' $
@@ -39,6 +40,14 @@ inc = mealy (\i _ -> (i+1, i) ) 0 (pure 0)
 
 simGPU = 
 	sequence $ 
-	fmap putStr $ 
-	P.concatMap (P.take 1) . P.iterate (P.drop 500) $ 
-	sample showFrame
+	fmap putStr  ( 
+	P.concatMap (P.take 1) . P.iterate (P.drop 50) $ 
+	sample showFrame )
+
+simGPUclr = 
+	sequence $ fmap (((>>) clearScreen) .  putStr)  ( 
+	P.concatMap (P.take 1) . P.iterate (P.drop 150) $ 
+	sample showFrame )
+
+
+
