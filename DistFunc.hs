ce0c7771d1918b5be117322ea3e16614e48003ba
Chi Luong <cl0udify@hotmail.com>
2017-04-19 14:17:35 +0200

    SIMD

--- DistFunc.hs
+++ DistFunc.hs
@@ -26,6 +26,8 @@ data Op
 	| Sqrt
 	| Abs
 	| Floor
+	| Dot
+	| Cross
 	deriving (Eq, Show, Generic, NFData)
 
 arity Sqrt = 1
@@ -33,13 +35,15 @@ arity Abs = 1
 arity Floor = 1
 arity _ = 2
 
-apply :: Op -> Float -> Float -> Float
-apply Max a b = max a b
-apply Min a b = min a b
-apply Add a b = a + b
-apply Sub a b = a - b
-apply Mul a b = a * b
-apply Div a b = a / b
-apply Sqrt a b = sqrt b
-apply Abs a b = abs b
-apply Floor a b = shiftL (shiftR b 16) 16
+apply :: Op -> Float -> Float -> Float -> Float -> Float -> Float -> Float
+apply Max a b _ _ _ _  = max a b
+apply Min a b _ _ _ _ = min a b
+apply Add a b _ _ _ _ = a + b
+apply Sub a b _ _ _ _ = a - b
+apply Mul a b _ _ _ _ = a * b
+apply Div a b _ _ _ _ = a / b
+apply Sqrt a b _ _ _ _ = sqrt b
+apply Abs a b _ _ _ _ = abs b
+apply Floor a b _ _ _ _  = shiftL (shiftR b 16) 16
+apply Dot a1 a2 a3 b1 b2 b3 = a1*b1 + a2*b2 + a3*b3
+apply Cross a1 a2 a3 b1 b2 b3 = 
