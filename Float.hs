0d83064db539a3a17ab08edd23370b117cd66367
Elias Forsberg <forsberg.elias@gmail.com>
2017-05-01 17:00:58 +0200

    Remove useless adders on right side of diagonal
    Also remove some type signatures because they are getting
    out of hand and ghc can't typecheck some of them anymore.

--- Float.hs
+++ Float.hs
@@ -3,7 +3,8 @@
 module Float where
 
 import CLaSH.Sized.Internal.Unsigned
-import Base
+import Base hiding (pack)
+import CLaSH.Prelude (pack)
 
 type GenFloat (a :: Nat) = Fixed Signed a a
 type Float = GenFloat 16
@@ -25,47 +26,27 @@ instance Floating (GenFloat 16) where
 	acosh = undefined
 	atanh = undefined
 
-topEntity = mealy (\st inp -> (\x -> (x,x)) $ sqrtFS inp + st) 0
+topEntity :: Signal Float -> Signal Float
+topEntity = mealy (\sum inp -> (\x -> (x,x)) $ sqrtFS inp + sum) 0 . register 0
 
 -- square root on 16 16 signed fixed numbers
 -- 24 iterations on 48 internal bits
 sqrtFS :: Float -> Float
 sqrtFS x = 
 	bitCoerce 
-	(resize# (shiftR (thd3 (loopN $ expand x)) 0)::Unsigned 32 )
+	(resize# (iSqrt48 $ expand x) ::Unsigned 32)
 	where
 		expand :: Fixed Signed 16 16 -> Unsigned 48
 		expand x = bitCoerce (resizeF x :: SFixed 16 32) :: Unsigned 48
-
-		thd3 (_,_,x) = x
-
-loop :: Unsigned 48 -> Vec 24 (Unsigned 48, Unsigned 48, Unsigned 48)
-loop x = generate d24 
-	loopStep
-	(startbit, x ,0) -- these are the starting values for bit, num, res
-	where 
-		startbit :: Unsigned 48
-		startbit = shiftL 1 46
-
-loopStep (bit, num, res) =
-	( bit `shiftR` 2
-	, if cond then temp else num
-	, if cond then resShift `or#` bit else resShift
-	)
-	where
-		temp = num - (res `or#` bit)		
-		cond = isNegative temp
-		resShift = res `shiftR` 1
 
 isNegative :: KnownNat n => Unsigned (n + 1) -> Bool
 isNegative x = (x `and#` (1 `shiftL` fromInteger (natVal $ y x))) == 0
 	where
 		y = undefined :: KnownNat n => Unsigned (n + 1) -> Unsigned n
 	
-type Step n = (Unsigned n, Unsigned n, Unsigned n)
-
-loopN :: Unsigned 48 -> Step 48
-loopN x =
+iSqrt48 :: Unsigned 48 -> Unsigned 48
+iSqrt48 x =
+	snd .
 	loopStepN d24 .
 	loopStepN d23 .
 	loopStepN d22 .
@@ -90,27 +71,36 @@ loopN x =
 	loopStepN d3 .
 	loopStepN d2 .
 	loopStepN d1 $
-	(startbit, x ,0) -- these are the starting values for bit, num, res
-	where 
-		startbit :: Unsigned 48
-		startbit = shiftL 1 46
+	(x ,0) -- these are the starting values for num, res
 
-loopStepN n (bit, num, res) =
-	( bit `shiftR` 2
-	, if cond then temp else num
-	, if cond then resShift `or#` bit else resShift
+loopStepN n (num, res) =
+	( if cond then temp else num
+	, res'
 	)
 	where
-		temp = subHighBits (mulSNat n d2) num (res `or#` bit)
+		temp = subHighBits n num (nextRes n'' res True)
 		cond = isNegative temp
 		resShift = res `shiftR` 1
+		n' = mulSNat n d2
+		n'' = subSNat n' d1
+		res' = nextRes n'' resShift cond
 
-subHighBits :: (KnownNat m, KnownNat n) =>
-	SNat n -> Unsigned (n + m) -> Unsigned (n + m) -> Unsigned (n + m)
+nextRes :: (KnownNat n, KnownNat m) => 
+	SNat n -> Unsigned (n + m + 1) -> Bool -> Unsigned (n + m + 1)
+nextRes n' r c = unpack (pack# (highBits n' r) ++# pack c ++# 0)
+
 subHighBits n a b = unpack $
 	(++#)
-	(pack# (highBits n a - highBits n b))
-	(pack# $ lowBits n a)
+	(pack (ah' - bh'))
+	(pack $ lowBits n' a)
+	where
+		n' = mulSNat n d2
+
+		ah = highBits n' a
+		bh = highBits n' b
+
+		ah' = (unpack $ 0 ++# (pack $ lowBits n ah)) `asTypeOf` ah
+		bh' = (unpack $ 0 ++# (pack $ lowBits n bh)) `asTypeOf` bh
 
 highBits :: (KnownNat n, KnownNat m) => SNat n -> Unsigned (n + m) -> Unsigned n
 highBits n = unpack . bTake n . pack#
